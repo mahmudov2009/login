095e357e623cbcb702b417e80e1f7d16381b09cf
mahmudov2009 <abdurahmonmahmudov2009@gmail.com>
2023-11-02 11:38:50 +0500

    first commit

---
+++ src/pages/birinchifail.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../css/birinchifail.css'
+export default function () {
+  return (
+    <div id='div21'>
+        <form id='o1' action="">
+         <label id='labe' htmlFor="">LoginForm</label>
+          <button id='lolq2'>Login</button>
+         <button id='lolq3'>Signup</button>
+          <input id='sigr' type="text" placeholder='email' />
+          <br />
+      <input id='mol' type="text"  placeholder='nomer'/>
+      <br />
+         <label id='o11' htmlFor="">Form dhuhdh?</label>
+         <br />
+         <button id='lolq4'>lelkle</button>
+        </form>
+    </div>
+  )
+}
